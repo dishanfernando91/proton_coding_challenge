bbf1322405f61bb9d18857b27e8434d233726f85
Dishan Fernando <dmfernando91@gmail.com>
2023-11-26 14:21:51 +0530

    disable navpag on traverse

--- src/components/NavPad/NavPad.tsx
+++ src/components/NavPad/NavPad.tsx
@@ -6,7 +6,7 @@ import ArrowSvg from "../../assets/arrow.svg";
 import "./styles.css";
 
 export default function NavPad() {
-  const { moveRobot } = useNavigation();
+  const { moveRobo, isTraversing } = useNavigation();
 
   return (
     <div className="container">
@@ -17,9 +17,9 @@ export default function NavPad() {
           style={{
             height: 60,
             width: 60,
-            cursor: "pointer",
+            cursor: isTraversing ? "not-allowed" : "pointer",
           }}
-          onClick={() => moveRobot(NAV_ACTIONS.UP)}
+          onClick={() => !isTraversing && moveRobot(NAV_ACTIONS.UP)}
         />
         <div className="middle-group">
           <img
@@ -29,9 +29,9 @@ export default function NavPad() {
               height: 60,
               width: 60,
               transform: "rotate(-90deg)",
-              cursor: "pointer",
+              cursor: isTraversing ? "not-allowed" : "pointer",
             }}
-            onClick={() => moveRobot(NAV_ACTIONS.LEFT)}
+            onClick={() => !isTraversing && moveRobot(NAV_ACTIONS.LEFT)}
           />
           <img
             src={ArrowSvg}
@@ -40,7 +40,7 @@ export default function NavPad() {
               height: 60,
               width: 60,
               transform: "rotate(90deg)",
-              cursor: "pointer",
+              cursor: isTraversing ? "not-allowed" : "pointer",
             }}
             onClick={() => moveRobot(NAV_ACTIONS.RIGHT)}
           />
@@ -52,13 +52,13 @@ export default function NavPad() {
             height: 60,
             width: 60,
             transform: "rotate(180deg)",
-            cursor: "pointer",
+            cursor: isTraversing ? "not-allowed" : "pointer",
           }}
-          onClick={() => moveRobot(NAV_ACTIONS.DOWN)}
+          onClick={() => !isTraversing && moveRobot(NAV_ACTIONS.DOWN)}
         />
       </div>
       <button
-        onClick={() => moveRobot(NAV_ACTIONS.RESET)}
+        onClick={() => !isTraversing && moveRobot(NAV_ACTIONS.RESET)}
         className="reset-button"
       >
         Reset Position
